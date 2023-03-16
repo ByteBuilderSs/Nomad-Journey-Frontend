96d42eb3694e735bf6432ce6d63781daf47a7e49
baktash81 <baktash.ansari1381@gmail.com>
2023-03-16 12:06:49 +0330

    [fix] : fix padding of three boxes

---
+++ src/components/MainPage/MainPage.jsx
@@ -0,0 +1,96 @@
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import ProgressBar from 'react-bootstrap/ProgressBar';
+import "./MainPage.css"
+
+function WithLabelExample() {
+    const now = 60;
+    return (<ProgressBar now={now} label={`${now}%`} />);
+}
+
+export default function MainPage(){
+
+
+    return(
+
+        <div >
+            
+            <div className='boxes'>
+
+                <div className='profile'>
+
+                    <Box
+                        sx={{
+                            width: 300,
+                            height: 160,
+                            backgroundColor: '#9B9B9B',
+                            '&:hover': {
+                            backgroundColor: "grey",
+                            opacity: [0.9, 0.8, 0.7],
+                            },
+                        }}
+                        >
+                            <div>
+                                <header className='profheader'>
+                                    <h1> <b> Name Family</b></h1>
+                                    <h2>Country</h2>
+                                </header>
+                            </div>
+                    </Box>
+
+                        <br/>
+
+                    <Box
+                        sx={{
+                            width: 300,
+                            height: 160,
+                            backgroundColor: '#9B9B9B',
+                            '&:hover': {
+                            backgroundColor: "grey",
+                            opacity: [0.9, 0.8, 0.7],
+                            },
+                        }}
+                        >
+                            <div>
+                                <header className='progheader'>
+                                    <h1> <b> My Profile</b></h1>
+                                </header>
+                                <div className='prog'>
+                                    <WithLabelExample />
+                                </div>
+                            </div>  
+                            <div className='complete'>
+                                <h3>Completed</h3>
+                            </div>          
+                    </Box>
+                    
+                </div>
+
+
+                <div className='random'>
+                    <Box
+                        sx={{
+                            width: 1000,
+                            height: 500,
+                            backgroundColor: '#9B9B9B',
+                            '&:hover': {
+                            backgroundColor: "grey",
+                            opacity: [0.9, 0.8, 0.7],
+                            },
+                        }}
+                        >    
+                        test    
+                    </Box>
+                </div>
+
+
+            </div>
+            
+            
+
+        </div>
+
+
+    );
+
+}
--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -1,22 +1,13 @@
+import MainPage from "../components/MainPage/MainPage";
 
-import RandomBox from "../components/MainPage/RandomSec";
-import ProfileBox from "../components/MainPage/ProfileSec";
-import ProgressBox from "../components/MainPage/ProgressSec"
 
 function MainPageFunc() {
     return (
 
         <div>
-            
-            <div style={{display: "inline"}}>
-                <ProfileBox />
-                <ProgressBox />
-            </div>
 
-            <div><RandomBox /></div>
-            
-            
-            
+                <MainPage />
+                           
         </div>
     
     );

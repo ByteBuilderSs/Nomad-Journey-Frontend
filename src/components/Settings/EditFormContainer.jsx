0d1198732d1e542c9b8c475c3d6217db20458ced
AylinNaebzadeh <aylin.naebzadeh@gmail.com>
2023-03-28 10:30:31 +0430

    [add] : initial file structers for settings page

---
+++ src/components/Settings/EditFormContainer.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import {
+    Card,
+} from '@mui/material';
+
+export default function FormContainer() {
+    return (
+        <div>
+            <Card>
+                
+            </Card>
+        </div>
+    )
+}
---
+++ src/components/Settings/ModalsContainer.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import {
+    Card,
+} from '@mui/material';
+
+export default function ModalsContainer() {
+    return (
+        <div>
+            <Card>
+                
+            </Card>
+        </div>
+    )
+}

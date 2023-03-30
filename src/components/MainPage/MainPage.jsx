c5b3eeeea23c9dbecbc732cf26ecf8da1ec0604f
baktash81 <baktash.ansari1381@gmail.com>
2023-03-31 01:31:30 +0330

    [add] : add raw announcement part in mainpage

--- src/components/MainPage/MainPage.jsx
+++ src/components/MainPage/MainPage.jsx
@@ -231,10 +231,205 @@ export default function MainPage(){
             </div>
           </nav>
         </div>
-    </section>
+      </section>
+
+      <div class="search-form">
+        <div class="container">
+          <div class="row">
+            <div class="col-lg-12">
+              <form id="search-form" name="gs" method="submit" role="search" action="#">
+                <div class="row">
+                  <div class="col-lg-2">
+                    <h4>Sort Announcements By:</h4>
+                  </div>
+                  <div class="col-lg-4">
+                      <fieldset>
+                          <select name="Location" class="form-select" aria-label="Default select example" id="chooseLocation" onChange="this.form.click()">
+                              <option selected>Destinations</option>
+                              <option type="checkbox" name="option1" value="Italy">Italy</option>
+                              <option value="France">France</option>
+                              <option value="Switzerland">Switzerland</option>
+                              <option value="Thailand">Thailand</option>
+                              <option value="Australia">Australia</option>
+                              <option value="India">India</option>
+                              <option value="Indonesia">Indonesia</option>
+                              <option value="Malaysia">Malaysia</option>
+                              <option value="Singapore">Singapore</option>
+                          </select>
+                      </fieldset>
+                  </div>
+                  <div class="col-lg-4">
+                      <fieldset>
+                          <select name="Price" class="form-select" aria-label="Default select example" id="choosePrice" onChange="this.form.click()">
+                              <option selected>Price Range</option>
+                              <option value="100">$100 - $250</option>
+                              <option value="250">$250 - $500</option>
+                              <option value="500">$500 - $1,000</option>
+                              <option value="1000">$1,000 - $2,500</option>
+                              <option value="2500+">$2,500+</option>
+                          </select>
+                      </fieldset>
+                  </div>
+                  <div class="col-lg-2">                        
+                      <fieldset>
+                          <button class="border-button">Search Results</button>
+                      </fieldset>
+                  </div>
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="amazing-deals">
+        <div class="container">
+          <div class="row">
+            <div class="col-lg-6 offset-lg-3">
+              <div class="section-heading text-center">
+                <h2>Best Weekly Offers In Each City</h2>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore.</p>
+              </div>
+            </div>
+            <div class="col-lg-6 col-sm-6">
+              <div class="item">
+                <div class="row">
+                  <div class="col-lg-6">
+                    <div class="image">
+                      <img src="../../Assets/images/deals-01.jpg" alt=""/>
+                    </div>
+                  </div>
+                  <div class="col-lg-6 align-self-center">
+                    <div class="content">
+                      <span class="info">*Limited Offer Today</span>
+                      <h4>Glasgow City Lorem</h4>
+                      <div class="row">
+                        <div class="col-6">
+                          <i class="fa fa-clock"></i>
+                          <span class="list">5 Days</span>
+                        </div>
+                        <div class="col-6">
+                          <i class="fa fa-map"></i>
+                          <span class="list">Daily Places</span>
+                        </div>
+                      </div>
+                      <p>Lorem ipsum dolor sit amet dire consectetur adipiscing elit.</p>
+                      <div class="main-button">
+                        <a href="reservation.html">Make a Reservation</a>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="col-lg-6 col-sm-6">
+              <div class="item">
+                <div class="row">
+                  <div class="col-lg-6">
+                    <div class="image">
+                      <img src="../../Assets/images/deals-02.jpg" alt=""/>
+                    </div>
+                  </div>
+                  <div class="col-lg-6 align-self-center">
+                    <div class="content">
+                      <span class="info">*Today & Tomorrow Only</span>
+                      <h4>Venezia Italy Ipsum</h4>
+                      <div class="row">
+                        <div class="col-6">
+                          <i class="fa fa-clock"></i>
+                          <span class="list">5 Days</span>
+                        </div>
+                        <div class="col-6">
+                          <i class="fa fa-map"></i>
+                          <span class="list">Daily Places</span>
+                        </div>
+                      </div>
+                      <p>Lorem ipsum dolor sit amet dire consectetur adipiscing elit.</p>
+                      <div class="main-button">
+                        <a href="reservation.html">Make a Reservation</a>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="col-lg-6 col-sm-6">
+              <div class="item">
+                <div class="row">
+                  <div class="col-lg-6">
+                    <div class="image">
+                      <img src="../../Assets/images/deals-03.jpg" alt=""/>
+                    </div>
+                  </div>
+                  <div class="col-lg-6 align-self-center">
+                    <div class="content">
+                      <span class="info">**Undefined</span>
+                      <h4>Glasgow City Lorem</h4>
+                      <div class="row">
+                        <div class="col-6">
+                          <i class="fa fa-clock"></i>
+                          <span class="list">5 Days</span>
+                        </div>
+                        <div class="col-6">
+                          <i class="fa fa-map"></i>
+                          <span class="list">Daily Places</span>
+                        </div>
+                      </div>
+                      <p>Lorem ipsum dolor sit amet dire consectetur adipiscing elit.</p>
+                      <div class="main-button">
+                        <a href="reservation.html">Make a Reservation</a>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="col-lg-6 col-sm-6">
+              <div class="item">
+                <div class="row">
+                  <div class="col-lg-6">
+                    <div class="image">
+                      <img src="../../Assets/images/deals-04.jpg" alt=""/>
+                    </div>
+                  </div>
+                  <div class="col-lg-6 align-self-center">
+                    <div class="content">
+                      <span class="info">*Offer Until 24th March</span>
+                      <h4>Glasgow City Lorem</h4>
+                      <div class="row">
+                        <div class="col-6">
+                          <i class="fa fa-clock"></i>
+                          <span class="list">5 Days</span>
+                        </div>
+                        <div class="col-6">
+                          <i class="fa fa-map"></i>
+                          <span class="list">Daily Places</span>
+                        </div>
+                      </div>
+                      <p>This free CSS template is provided by Template Mo website.</p>
+                      <div class="main-button">
+                        <a href="reservation.html">Make a Reservation</a>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="col-lg-12">
+              <ul class="page-numbers">
+                <li><a href="#"><i class="fa fa-arrow-left"></i></a></li>
+                <li><a href="#">1</a></li>
+                <li class="active"><a href="#">2</a></li>
+                <li><a href="#">3</a></li>
+                <li><a href="#"><i class="fa fa-arrow-right"></i></a></li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
 
 
-  </div>
+    </div>
 
 
 

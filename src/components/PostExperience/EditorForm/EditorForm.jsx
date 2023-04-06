2a5318b31653f7006ed7da145e5bcf628c27b659
AylinNaebzadeh <aylin.naebzadeh@gmail.com>
2023-04-06 17:58:44 +0430

    [add][fix] : blog page finalllyyy😭🙏🙏

--- src/components/PostExperience/EditorForm/EditorForm.jsx
+++ src/components/PostExperience/EditorForm/EditorForm.jsx
@@ -2,38 +2,27 @@ import React, { useState } from 'react';
 import { useEffect } from 'react';
 import ReactQuill, { Quill } from 'react-quill';
 import 'react-quill/dist/quill.snow.css';
-
 import {
-    Container,
     Grid,
-    Box,
-    Paper,
     FormControl,
     TextField,
     Button,
-    Divider,
-    InputLabel,
-    MenuItem,
     Stack,
     Card,
     Typography,
-    IconButton,
-    Chip,
     Autocomplete,
-    CircularProgress,
     Checkbox,
-    
 } from '@mui/material';
+
 import CheckBoxOutlineBlankIcon from '@mui/icons-material/CheckBoxOutlineBlank';
 import CheckBoxIcon from '@mui/icons-material/CheckBox';
 import { Item } from "semantic-ui-react";
 import axios from 'axios';
 import { toast } from "react-toastify";
+import {useNavigate} from "react-router-dom";
 
 const icon = <CheckBoxOutlineBlankIcon fontSize="small" />;
 const checkedIcon = <CheckBoxIcon fontSize="small" />;
-
-let Tags = [];
 
 function sleep(delay = 0) {
     return new Promise((resolve) => {
@@ -56,41 +45,20 @@ const modules = {
     ],
 }
 const EditorForm = () => {
+    let allData;
+    let access_token;
+    if (localStorage.getItem('tokens'))
+    {
+        allData = JSON.parse(localStorage.getItem('tokens'));
+        access_token = allData.access;
+    }
+    const navigate = useNavigate()
+    const [disabled, setDisabled] = useState(false);
     const [title, setTitle] = useState('');
     // this value is for editor
     const [editorValue, setEditorValue] = useState('');
     const [selectedTags, setSelectedTags] = useState([]);
-    const [open, setOpen] = useState(false);
-    const [options, setOptions] = useState([]);
-
-    const loading = open && options.length === 0;
-
-    useEffect(() => {
-        let active = true;
-    
-        if (!loading) {
-        return undefined;
-        }
-    
-        (async () => {
-        await sleep(1e3); // For demo purposes.
-    
-        if (active) {
-            setOptions([...Tags]);
-        }
-        })();
-    
-        return () => {
-        active = false;
-        };
-    }, [loading]);
-
-    useEffect(() => {
-        if (!open) {
-            setOptions([]);
-        }
-    }, [open]);
-
+    const [tags, setTags] = useState([]);
     const loadTags = async () => {
         axios({
             method: "get",
@@ -99,12 +67,8 @@ const EditorForm = () => {
                 'Content-Type': 'application/json',
             }
         }).then((result) => {
-            // console.log(result.data.data);
-            let data = result.data.data;
-            for (let i = 0; i < data.length; i++) {
-                Tags.push(data[i]);
-            }
-            console.log("The Tags is: ", Tags);
+            setTags(result.data.data);
+            console.log("********** The Tags are ******** ", tags);
         }).catch((error) => {
             toast.error("Something went wrong while fetching tags.")
         })
@@ -112,44 +76,81 @@ const EditorForm = () => {
 
     useEffect(() => {
         loadTags();
+        
     }, []);
 
     function onSubmit(e) {
         e.preventDefault();
-        console.log(editorValue);
+        let tagIDs = []
+        for (let i=0; i < selectedTags.length; i++)
+        {
+            tagIDs.push(selectedTags[i].uid);
+        }
         let isDataValid = true;
         if (!title) {
             toast.error("Please select a title for your post.");
             isDataValid = false;
         }
-        if (!editorValue) {
+        console.log("***** The editor content ****", editorValue.ops[0].insert);
+        if (!editorValue.ops[0].insert || 
+            editorValue.ops[0].insert === undefined ||
+            editorValue.ops[0].insert === null ||
+            editorValue.ops[0].insert.length === 0 || 
+            editorValue.ops[0].insert === "\n" || 
+            editorValue.ops[0].insert === "\t" ||
+            (editorValue.ops[0].insert && editorValue.ops[0].insert.trim().length === 0)) {
             toast.error("No content!?");
             isDataValid = false;
         }
         if (isDataValid) {
             axios({
                 method: "post",
-                url: "",
+                url: "http://127.0.0.1:8000/api/v1/blog/userpost/",
                 headers: {
-                    
+                    'Content-Type': 'application/json',
+                    'Authorization': `Bearer ${access_token}`
                 },
                 data: {
-                    
+                    blog_title: title,
+                    json_data: editorValue,
+                    tags: tagIDs
                 },
                 }).then((res) => {
-                    
+                    setDisabled(true);
+                    setTimeout(() => {
+                        setDisabled(false);
+                    }, 5000);
+                    toast.success("A new post created successfully.");
+                    setTitle('');
+                    setEditorValue('');
+                    setSelectedTags([]);
+                }).catch((error) => {
+                    toast.error("Something went wrong.")
                 });
         }
     }
 
+    const handleChangeTitle = (event) => {
+        setTitle(event.target.value);
+    }
     // onChange expects a function with these 4 arguments
-    function handleChangeEditorContent(content, delta, source, editor) {
+    const handleChangeEditorContent = (content, delta, source, editor) => {
         setEditorValue(editor.getContents());
     }
+    // select tag
+    const handleTagSelection = (values) => {
+        console.log("********** The value is: ***********", values);
+        setSelectedTags(values);
+    }
+
+    console.log("++++++++ The selected tags are: ++++++++", selectedTags);
+
+
     return (
         <div>
             <Card dir='ltr'>
                 <form>
+                
                     <Grid container rowSpacing={1} columnSpacing={{ xs: 1, sm: 2, md: 3 }}>
                         <div style={{ paddingLeft: "10rem" }}>
                             {/* Title */}
@@ -173,6 +174,8 @@ const EditorForm = () => {
                                                 type={"text"}
                                                 placeholder='e.g. My memories of a trip to the southern parts of Italy'
                                                 size='small'
+                                                value={title}
+                                                onChange={handleChangeTitle}
                                                 required
                                             />
                                         </FormControl>
@@ -213,32 +216,34 @@ const EditorForm = () => {
                                         </Typography>
                                     </Item>
                                     <Item>
-                                        <FormControl s>
+                                        <FormControl>
                                             <Autocomplete
+                                                clearIcon={false}
                                                 multiple
                                                 id="tags-outlined"
-                                                open={open}
-                                                onOpen={() => {
-                                                    setOpen(true);
+                                                options={tags}
+                                                value={selectedTags}
+                                                isOptionEqualToValue={(option, value) => option.tag_name === value.tag_name}
+                                                getOptionSelected={(option, value) => {
+                                                    return option.tag_name === value.tag_name;
                                                 }}
-                                                onClose={() => {
-                                                    setOpen(false);
-                                                }}
-                                                // isOptionEqualToValue={(option, value) => option.title === value.title}
-                                                loading={loading}
-                                                options={Tags}
                                                 getOptionLabel={(option) => option.tag_name}
-                                                filterSelectedOptions
                                                 sx={{ width: "50rem" }}
                                                 size="small"
                                                 disableCloseOnSelect
+                                                noOptionsText="No related tag is available"
+                                                onChange={(e, values) => {
+                                                    handleTagSelection(values);
+                                                }}
                                                 renderOption={(props, option, { selected }) => (
                                                     <li {...props}>
                                                         <Checkbox
                                                         icon={icon}
                                                         checkedIcon={checkedIcon}
                                                         style={{ marginRight: 8 }}
-                                                        checked={selected}
+                                                        checked={
+                                                            selected
+                                                        }
                                                         />
                                                         {option.tag_name}
                                                     </li>
@@ -261,21 +266,23 @@ const EditorForm = () => {
                                             sx={{ width: "100%", backgroundColor: "#088AD1" }}
                                             type="submit"
                                             onClick={onSubmit}
+                                            disabled={disabled}
                                         >
-                                            Update
+                                            Add New Post
                                         </Button>
                                     </Item>
-                                    <Item>
+                                    {/* <Item>
                                         <Button
                                             variant="outlined"
                                             sx={{
                                                 width: "100%",
                                             }}
                                             type="submit"
+                                            onClick={onCancel}
                                         >
                                             Cancel
                                         </Button>
-                                    </Item>
+                                    </Item> */}
                                 </Stack>
                             </Grid>
                         </div>

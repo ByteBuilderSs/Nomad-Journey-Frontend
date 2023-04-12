e0585588b8aa8b432fff66e2099f511d3fdacf98
AylinNaebzadeh <aylin.naebzadeh@gmail.com>
2023-04-12 19:20:07 +0430

    [add] : some changes to posts card + each post page

--- src/components/UserPanel/RightBar/myPosts/MyPosts.jsx
+++ src/components/UserPanel/RightBar/myPosts/MyPosts.jsx
@@ -21,7 +21,10 @@ import {useNavigate} from "react-router-dom";
 import { CgDetailsMore } from "react-icons/cg";
 import { blue, yellow, red, deepOrange } from '@mui/material/colors';
 import { createTheme, ThemeProvider } from "@mui/material/styles";
-
+import { AiOutlineFieldTime, AiFillDelete, AiFillLike } from "react-icons/ai";
+import TitleIcon from '@mui/icons-material/Title';
+import { FcList } from "react-icons/fc";
+import {BsCalendarDateFill, BsFillEyeFill} from "react-icons/bs";
 
 const theme = createTheme({
   palette: {
@@ -43,6 +46,10 @@ const AllPosts=({blogs})=>
   {
     navigate("/home/PostExperience/");
   };
+  const handleDeletePostClick = () => {
+
+  };
+
   return (
   <ThemeProvider theme={theme}>
     <Box
@@ -64,7 +71,7 @@ const AllPosts=({blogs})=>
           Add new post
         </Button>
         <Box sx={{ marginTop: 1 }}>
-          <Grid sx={{ marginTop: 1 }} container spacing={2}>
+          <Grid sx={{ marginTop: 1 }} container spacing={1}>
             {
               blogs.length > 0 ? (
                 blogs.map((blog)=>( 
@@ -78,7 +85,7 @@ const AllPosts=({blogs})=>
                     xs={12}
                     // sx={{ m: 1 }}
                   >
-                  <Card className="users-card">
+                  <Card className="posts-card">
                     <CardContent>
                       <Box
                         sx={{
@@ -89,18 +96,19 @@ const AllPosts=({blogs})=>
                       >
                         <Box >
                           <Typography
-                            sx={{ fontSize: 14 }}
+                            sx={{ fontSize: 25, fontWeight: "bold", display: "flex", alignItems: "center" }}
                             variant="h1"
                             component="div"
                           >
-                            Title: {blog.blog_title}
+                            <FcList size='2rem' style={{ marginRight: "0.5rem" }}/>
+                            {blog.blog_title}
                           </Typography>
                         </Box>
                       </Box>
                       <Box sx={{ mt: "0.75rem" }}>
                           <Typography
                             sx={{ marginTop: "1rem", fontSize: 14, display: "flex", alignItems: "center" }}
-                            variant="h1"
+                            variant="p"
                             component="div"
                           >
                             Tags:
@@ -108,28 +116,47 @@ const AllPosts=({blogs})=>
                                     useFlexGap 
                                     flexWrap="wrap"
                                     spacing={1} 
-                                    sx={{ ml: "1rem", '& > *': { flexGrow: 1 } }}>
+                                    sx={{ ml: "1rem", '& > *': { flexGrow: 2 } }}>
                                 {
-                                  blog.tags_name.map((tag_name) => (
+                                  blog.tags_name.length > 0  ? (blog.tags_name.map((tag_name) => (
                                     <Chip label={tag_name} color="primary" variant="outlined" sx={{ mb: "0.25rem"}}/>
-                                  ))
+                                  ))) : (
+                                    <span> No related tag found </span>
+                                  )
                                 }
                               </Stack>
                           </Typography>
                       </Box>
-      
                       <Typography
-                        sx={{ marginTop: "1rem", fontSize: 14 }}
+                        sx={{ marginTop: "1rem", fontSize: 16, display: "flex", alignItems: "center"  }}
                         variant="p"
                         component="div"
                       >
-                        created date: { blog.created_at }
+                        <AiFillLike style={{ marginRight: "0.5rem" }} /> # Likes
+                      </Typography>
+
+                      <Typography
+                        sx={{ marginTop: "1rem", fontSize: 16, display: "flex", alignItems: "center"  }}
+                        variant="p"
+                        component="div"
+                      >
+                        <BsFillEyeFill style={{ marginRight: "0.5rem" }} /> # Views
+                      </Typography>
+
+                      <Typography
+                        sx={{ marginTop: "1rem", fontSize: 16, display: "flex", alignItems: "center" }}
+                        variant="p"
+                        component="div"
+                      >
+                        <BsCalendarDateFill style={{ marginRight: "0.5rem" }}/>
+                        Created at: { blog.created_at }
                       </Typography>
                     </CardContent>
+
                     <CardActions
                       sx={{
                         display: "flex",
-                        justifyContent: "flex-end",
+                        justifyContent: "space-between",
                         alignItems: "center",
                       }}
                     >
@@ -141,6 +168,19 @@ const AllPosts=({blogs})=>
                             }
                             color="#b9b8b8"
                             style={{ cursor: "pointer" }}
+                            size='2rem'
+                          />
+                        </div>
+                      </Tooltip>
+                      <Tooltip title="Delete this post" arrow>
+                        <div>
+                          <AiFillDelete
+                            onClick={() =>
+                              handleDeletePostClick()
+                            }
+                            color="#b9b8b8"
+                            style={{ cursor: "pointer" }}
+                            size='2rem'
                           />
                         </div>
                       </Tooltip>

f8885bf5f127c44b2c50699fc96df927867180ac
AylinNaebzadeh <aylin.naebzadeh@gmail.com>
2023-04-12 12:01:12 +0430

    [add] : button in user posts

--- src/components/UserPanel/RightBar/myPosts/MyPosts.jsx
+++ src/components/UserPanel/RightBar/myPosts/MyPosts.jsx
@@ -12,15 +12,23 @@ import {
   Tooltip,
   Chip,
   Stack,
+  Divider,
 } from '@mui/material';
-import ReactQuill, { Quill } from 'react-quill';
 import 'react-quill/dist/quill.bubble.css';
 import './MyPosts.css'
 import {useMyBlogs} from '../../../../hooks/useMyBlogs';
-import axios from 'axios';
 import {useNavigate} from "react-router-dom";
 import { CgDetailsMore } from "react-icons/cg";
-import DatePicker, { DateObject } from "react-multi-date-picker";
+import { blue, yellow, red, deepOrange } from '@mui/material/colors';
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+
+
+const theme = createTheme({
+  palette: {
+    primary: blue,
+    secondary: deepOrange
+  }
+});
 
 
 const AllPosts=({blogs})=> 
@@ -31,9 +39,12 @@ const AllPosts=({blogs})=>
   {
     navigate(`/home/PostExperience/${slug}`);
   };
-
+  const handleNewPostRoute = () =>
+  {
+    navigate("/home/PostExperience/");
+  };
   return (
-  <>
+  <ThemeProvider theme={theme}>
     <Box
       sx={{
         flexGrow: 1,
@@ -42,104 +53,114 @@ const AllPosts=({blogs})=>
       }}
     >
       <div>
-      <Box sx={{ marginTop: 1 }}>
-        <Grid sx={{ marginTop: 1 }} container spacing={2}>
-          {
-            blogs.length > 0 ? (
-              blogs.map((blog)=>( 
-                <Grid
-                  key={blog.slug}
-                  item
-                  xl={4}
-                  lg={4}
-                  md={4}
-                  sm={6}
-                  xs={12}
-                  // sx={{ m: 1 }}
-                >
-                <Card className="users-card">
-                  <CardContent>
-                    <Box
+        <Button
+          sx={{ marginLeft: "48.25rem" }}
+          variant="contained"
+          size="medium"
+          style={{ minWidth: 150 }}
+          color="secondary" 
+          onClick={handleNewPostRoute}
+          >
+          Add new post
+        </Button>
+        <Box sx={{ marginTop: 1 }}>
+          <Grid sx={{ marginTop: 1 }} container spacing={2}>
+            {
+              blogs.length > 0 ? (
+                blogs.map((blog)=>( 
+                  <Grid
+                    key={blog.slug}
+                    item
+                    xl={12}
+                    lg={12}
+                    md={12}
+                    sm={12}
+                    xs={12}
+                    // sx={{ m: 1 }}
+                  >
+                  <Card className="users-card">
+                    <CardContent>
+                      <Box
+                        sx={{
+                          display: "flex",
+                          alignItems: "center",
+                          justifyContent: "space-between",
+                        }}
+                      >
+                        <Box >
+                          <Typography
+                            sx={{ fontSize: 14 }}
+                            variant="h1"
+                            component="div"
+                          >
+                            Title: {blog.blog_title}
+                          </Typography>
+                        </Box>
+                      </Box>
+                      <Box sx={{ mt: "0.75rem" }}>
+                          <Typography
+                            sx={{ marginTop: "1rem", fontSize: 14, display: "flex", alignItems: "center" }}
+                            variant="h1"
+                            component="div"
+                          >
+                            Tags:
+                              <Stack direction="row" 
+                                    useFlexGap 
+                                    flexWrap="wrap"
+                                    spacing={1} 
+                                    sx={{ ml: "1rem", '& > *': { flexGrow: 1 } }}>
+                                {
+                                  blog.tags_name.map((tag_name) => (
+                                    <Chip label={tag_name} color="primary" variant="outlined" sx={{ mb: "0.25rem"}}/>
+                                  ))
+                                }
+                              </Stack>
+                          </Typography>
+                      </Box>
+      
+                      <Typography
+                        sx={{ marginTop: "1rem", fontSize: 14 }}
+                        variant="p"
+                        component="div"
+                      >
+                        created date: { blog.created_at }
+                      </Typography>
+                    </CardContent>
+                    <CardActions
                       sx={{
                         display: "flex",
+                        justifyContent: "flex-end",
                         alignItems: "center",
-                        justifyContent: "space-between",
                       }}
                     >
-                      <Box >
-                        <Typography
-                          sx={{ fontSize: 14 }}
-                          variant="h1"
-                          component="div"
-                        >
-                          Title: {blog.blog_title}
-                        </Typography>
-                      </Box>
-                    </Box>
-                    <Box sx={{ mt: "0.75rem" }}>
-                        <Typography
-                          sx={{ marginTop: "1rem", fontSize: 14, display: "flex", alignItems: "center" }}
-                          variant="h1"
-                          component="div"
-                        >
-                          Tags:
-                            <Stack direction="row" 
-                                  useFlexGap 
-                                  flexWrap="wrap"
-                                  spacing={1} 
-                                  sx={{ ml: "1rem", '& > *': { flexGrow: 1 } }}>
-                              {
-                                blog.tags_name.map((tag_name) => (
-                                  <Chip label={tag_name} color="primary" variant="outlined" sx={{ mb: "0.25rem"}}/>
-                                ))
-                              }
-                            </Stack>
-                        </Typography>
-                    </Box>
-    
-                    <Typography
-                      sx={{ marginTop: "1rem", fontSize: 14 }}
-                      variant="p"
-                      component="div"
-                    >
-                      created date: { blog.created_at }
-                    </Typography>
-                  </CardContent>
-                  <CardActions
-                    sx={{
-                      display: "flex",
-                      justifyContent: "flex-end",
-                      alignItems: "center",
-                    }}
-                  >
-                    <Tooltip title="Details" arrow>
-                      <div>
-                        <CgDetailsMore
-                          onClick={() =>
-                            handleDetailsClick(blog.slug)
-                          }
-                          color="#b9b8b8"
-                          style={{ cursor: "pointer" }}
-                        />
-                      </div>
-                    </Tooltip>
-                  </CardActions>
-                </Card>
-              </Grid>
-              ))
-            ) :
-            (
-              <span style={{ marginLeft: "25rem" , fontWeight: "bold", fontSize: 20}}>
-                No Posts Found!
-              </span>
-            )
-          }
-        </Grid>
-      </Box>
+                      <Tooltip title="Details" arrow>
+                        <div>
+                          <CgDetailsMore
+                            onClick={() =>
+                              handleDetailsClick(blog.slug)
+                            }
+                            color="#b9b8b8"
+                            style={{ cursor: "pointer" }}
+                          />
+                        </div>
+                      </Tooltip>
+                    </CardActions>
+                  </Card>
+                </Grid>
+                ))
+              ) :
+              (
+                <span style={{ marginLeft: "25rem" , fontWeight: "bold", fontSize: 20}}>
+                  No Posts Found!
+                </span>
+              )
+            }
+          </Grid>
+        </Box>
       </div>
 
     </Box>
-  </>
+  </ThemeProvider>
   );
 }
 export default function MyPosts()

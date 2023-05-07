0a6cc0bd079aaf567f9747e2c31d354329a53f87
AylinNaebzadeh <aylin.naebzadeh@gmail.com>
2023-05-07 21:51:57 +0430

    [add] : send profile image to backend

--- src/components/UserPanel/EditProfile/SideBarCard.jsx
+++ src/components/UserPanel/EditProfile/SideBarCard.jsx
@@ -19,59 +19,114 @@ import {
 import { Item } from "semantic-ui-react";
 import DeleteIcon from '@mui/icons-material/Delete';
 import EditIcon from '@mui/icons-material/Edit';
+import CheckIcon from '@mui/icons-material/Check';
 import LetteredAvatar from 'react-lettered-avatar';
+import { useState } from 'react';
+import axios from 'axios';
+import { toast } from "react-toastify";
 
 const SideBarCard = () => {
-    let username = JSON.parse(localStorage.getItem('tokens')).username
+    const Data = JSON.parse(localStorage.getItem('tokens'));
+    const username = Data.username;
+    const access_token = Data.access;
+    const [profileImage, setProfileImage] = useState(null);
+    const [profileImageCode, setProfileImageCode] = useState("");
+
+    const handleInputFile = (event) => {
+        setProfileImage(event.target.files);
+    }
+
+    const handleUploadClick = async (event) => {
+        event.preventDefault();
+
+        let formData = new FormData();
+        
+        formData.append('profile_photo', profileImage[0]);
+        
+        console.log("************* THE FORM DATA IS ************* ", formData);
+        axios({
+            method: "patch",
+            url: `http://188.121.102.52:8000/api/v1/accounts/UserProfileEdit4/${username}`,
+            headers: {
+                'Authorization': `Bearer ${access_token}`
+            },
+            data : formData
+        }).then((res) => {
+            console.log(res);
+            toast.success("Changes updated successfully.");
+        }).catch((error) => {
+            toast.error("Something went wrong while updating information.");
+            console.log(error);
+        })
+    }
 
     return (
         <div>
             <Card>
-                <Stack spacing={6} sx={{ paddingBottom: "1rem" }}>
-                    <Item>
-                        <Stack alignItems={`center`} spacing={1}>
-                            <Item>
-                                <IconButton component="label">
-                                    <input
-                                        hidden
-                                        accept="image/*"
-                                        multiple
-                                        type="file"
-                                        max={20}
-                                    />
-                                    {/* <Avatar sx={{ width:'15rem', height:'15rem' }} /> */}
-                                    <LetteredAvatar name={username} backgroundColor='#FFE5B4' size={100} />
-                                </IconButton>
-                            </Item>
-                            <Item>
-                                <Button
-                                    sx={{ width: "100%" }}
-                                    variant="contained"
-                                    component="label"
-                                    startIcon={<EditIcon />}
-                                    >
-                                    Upload a photo
-                                    <input
-                                        hidden
-                                        accept="image/*"
-                                        multiple
-                                        type="file"
-                                    />
-                                </Button>
-                            </Item>
-                            <Item>
-                                <Button
-                                    sx={{ mt: 1, width: "100%" }}
-                                    variant="contained"
-                                    color="error"
-                                    startIcon={<DeleteIcon />}
-                                    >
-                                    Remove photo
-                                </Button>
-                            </Item>
-                        </Stack>
-                    </Item>
-                </Stack>
+                <form enctype="multipart/form-data">
+                    <Stack spacing={6} sx={{ paddingBottom: "1rem" }}>
+                        <Item>
+                            <Stack alignItems={`center`} spacing={1}>
+                                <Item>
+                                    <IconButton component="label">
+                                        <input
+                                            onChange={handleInputFile}
+                                            hidden
+                                            accept="image/*"
+                                            multiple
+                                            type="file"
+                                            max={20}
+                                        />
+                                        {/* <Avatar sx={{ width:'15rem', height:'15rem' }} /> */}
+                                        <LetteredAvatar name={username} backgroundColor='#FFE5B4' size={100} />
+                                    </IconButton>
+                                </Item>
+                                <Stack direction={'row'} spacing={1}>
+                                    <Item>
+                                        <Button
+                                            sx={{ width: "100%", textTransform: "none" }}
+                                            variant="outlined"
+                                            component="label"
+                                            startIcon={<EditIcon />}
+                                            size='small'
+                                            >
+                                            Upload a photo
+                                            <input
+                                                onChange={handleInputFile}
+                                                hidden
+                                                accept="image/*"
+                                                multiple
+                                                type="file"
+                                            />
+                                        </Button>
+                                    </Item>
+                                    <Item>
+                                        <Button
+                                            sx={{  width: "100%", textTransform: "none" }}
+                                            variant="outlined"
+                                            color="error"
+                                            startIcon={<DeleteIcon />}
+                                            size='small'
+                                            >
+                                            Remove photo
+                                        </Button>
+                                    </Item>
+                                </Stack>
+                                <Item>
+                                    <Button
+                                        sx={{ mt: 2, width: "100%", textTransform: "none" }}
+                                        variant="contained"
+                                        color="success"
+                                        startIcon={<CheckIcon />}
+                                        onClick={handleUploadClick}
+                                        >
+                                        Save Changes
+                                    </Button>
+                                </Item>
+                            </Stack>
+                        </Item>
+                    </Stack>
+                </form>
             </Card>
         </div>
     )

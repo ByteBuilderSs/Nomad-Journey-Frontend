e8617067c52fc754e86fbbeb911a068fa0015469
AylinNaebzadeh <a_naeb@comp.iust.ac.ir>
2023-07-06 20:34:27 +0330

    [fix] : bug in user panel while rendering profile photo

--- src/components/Announcements/AnnouncementDetails/Authenticated/UserProfileAnnouncement.jsx
+++ src/components/Announcements/AnnouncementDetails/Authenticated/UserProfileAnnouncement.jsx
@@ -2,25 +2,31 @@ import React, {useEffect, useMemo, useState} from "react";
 import axios from "axios";
 import {toast} from "react-toastify";
 import LetteredAvatar from "react-lettered-avatar";
+import { useCounter } from "../../../../Context/CounterProvider";
+
 
 export default function UserProfile({user_id, first_name, imageSize, profileSize})
 {
     const [profileImageURL, setProfileImageURL] = useState("");
-    useEffect(() => {
-        axios({
-            method: "get",
-            url: `https://api.nomadjourney.ir/api/v1/accounts/get-profile-photo/${user_id}`,
-            headers: {
-                'Content-Type': 'application/json',
-            }
-        }).then((result) => {
-            console.log("+++++++++ THE RESULT IS ++++++++ ", result);
-            if (result.data.profile_photo_URL && result.data.profile_photo_URL != "" ) {
-                setProfileImageURL("https://api.nomadjourney.ir" + result.data.profile_photo_URL);
-            }
+    const counter = useCounter();
 
-        })
-    }, []);
+    useEffect(() => {
+        if (user_id !== "" && user_id) {
+            axios({
+                method: "get",
+                url: `https://api.nomadjourney.ir/api/v1/accounts/get-profile-photo/${user_id}`,
+                headers: {
+                    'Content-Type': 'application/json',
+                }
+            }).then((result) => {
+                console.log("+++++++++ THE RESULT IS ++++++++ ", result);
+                if (result.data.profile_photo_URL && result.data.profile_photo_URL != "" ) {
+                    setProfileImageURL("https://api.nomadjourney.ir" + result.data.profile_photo_URL);
+                }
+            })
+        }
+    }, [counter, user_id]);
+    
     return(
         <>
             {

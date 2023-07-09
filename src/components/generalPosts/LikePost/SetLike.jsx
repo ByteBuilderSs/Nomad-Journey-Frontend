6bd3e9441bececc53bf5b0dc463ac94f5b50e805
Hanna Hashemi <hannahhashemi952@gmail.com>
2023-07-09 15:49:29 +0330

    general post

--- src/components/generalPosts/LikePost/SetLike.jsx
+++ src/components/generalPosts/LikePost/SetLike.jsx
@@ -1,10 +1,11 @@
 import React, { useEffect, useState } from 'react';
 import {AiFillHeart, AiOutlineHeart} from "react-icons/ai";
 import {useCounter, useCounterActions} from "../../../Context/CounterProvider";
-import {IconButton} from "@mui/material";
+import {IconButton, Typography,Grid} from "@mui/material";
 import {makeStyles} from "@mui/styles";
 import { useLikePost } from '../../../hooks/useLikePost';
-
+import '../generalPostsTab.css'
+import PostLikers from '../likerOfPost';
 
 const useStyles = makeStyles(theme => (
     {
@@ -24,31 +25,53 @@ const useStyles = makeStyles(theme => (
 
     }
 ));
+
+
 export default function SetLikeOfBlog(props)
 {
+    const [open_liker, setOpen_liker] = useState(false);
+    const [close_liker, setClose_liker] = useState(true);
+    const [blog_id, setBlog_id] = useState(null);
+
     const setCounter = useCounterActions();
     const counter = useCounter();
 
     const classes = useStyles();
     
     const[like,setLike]=useState(props.Isfill)
-
+    const [count,setCount]=useState(props.num)
     const {likepost,unlikepost}=useLikePost()
     const onSubmit =  () => {
         if(!like)
         {
             likepost(props.blog_id);
-            setCounter(counter+1)
+            // setCounter(counter+1)
+            setCount(count+1)
         }
         else
         {
             unlikepost(props.blog_id)
-            setCounter(counter-1)
+            setCount(count-1)
+            // setCounter(counter-1)
 
         }
         
     }
-   
+    const showModal=()=>
+    {
+        if(blog_id != null)
+                return(
+                    <>
+                        <PostLikers
+                            blog_id={props.blog_id}
+                            setBlog_id={setBlog_id}
+                            close={close_liker}
+                            setClose={setClose_liker}
+                            open={open_liker}
+                            setOpen={setOpen_liker}
+                        />
+                    </>)
+    }
     
 
     return(
@@ -58,9 +81,20 @@ export default function SetLikeOfBlog(props)
             
             <IconButton  onClick={()=>{onSubmit();setLike(!like)}}>
                 {like?<AiFillHeart className={classes.likeButtonActive}/>:<AiOutlineHeart className={classes.likeButton}/>}
+                
             </IconButton>
-            
-         
+            <Grid className="likes"
+                sx={{marginTop:'0.6rem'}}
+                onClick={()=>{
+                    setOpen_liker(true);
+                    setClose_liker(false);
+                    setBlog_id(props.blog_id)
+                }}
+                >
+                    {count > 0 ? count : null}
+            </Grid>
+         {showModal}
+         {()=>setBlog_id(null)}
         </>
     )
 }
